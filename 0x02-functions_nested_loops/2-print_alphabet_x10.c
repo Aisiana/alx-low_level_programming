27e9b1bf2f24b8cfede1dc0dc570207ec1c11836
Aisiana <aisiana254@gmail.com>
2022-07-05 14:00:31 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,47 +1,47 @@
-#include "holberton.h"
+nclude "holberton.h"
 
 
 
 /**
-
-  * print_alphabet_x10 - Make alphabet x10 times
-
-  *
-
-  * Return: void
-
-  */
+ *
+ *   * print_alphabet_x10 - Make alphabet x10 times
+ *
+ *     *
+ *
+ *       * Return: void
+ *
+ *         */
 
 void print_alphabet_x10(void)
 
 {
 
-	char c;
+		char c;
 
-	int i = 0;
-
-
-
-	while (i <= 9)
-
-	{
-
-		for (c = 'a'; c <= 'z'; c++)
-
-		{
-
-			_putchar(c);
-
-		}
+			int i = 0;
 
 
 
-		_putchar('\n');
+				while (i <= 9)
+
+						{
+
+									for (c = 'a'; c <= 'z'; c++)
+
+												{
+
+																_putchar(c);
+
+																		}
 
 
 
-		i++;
+											_putchar('\n');
 
-	}
+
+
+													i++;
+
+														}
 
 }
